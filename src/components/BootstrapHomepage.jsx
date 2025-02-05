d1375fc775a585af75dfc6c682472ccdd43269b7
ragi <ragisapovadia@gmail.com>
2025-02-05 16:20:54 +0530

    done

--- src/components/BootstrapHomepage.jsx
+++ src/components/BootstrapHomepage.jsx
@@ -111,7 +111,7 @@ export default function BootstrapHomepage() {
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div> 
-            </div>
+            </div> 
 
             <div className="footer overflow-hidden mt-5  bg-light pt-5">
                 <div className="row justify-content-center">
