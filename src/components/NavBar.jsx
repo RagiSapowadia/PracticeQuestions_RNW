b6bd9a06aa365d23a5614704ae1c25ac2737e330
ragi <ragisapovadia@gmail.com>
2025-02-16 11:25:44 +0530

    done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      <Link to="/">Card</Link>
+      <Link to="/">Card</Link> 
       <Link to="/todo">Todo</Link>
       <Link to="/controlled_form">Controlled Form</Link>
       <Link to="/categories">Categories</Link>
