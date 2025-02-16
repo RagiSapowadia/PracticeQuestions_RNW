80222da6fb747702fbb4b540e60b199c26e00cf4
ragi <ragisapovadia@gmail.com>
2025-02-16 11:15:52 +0530

    done

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link, Outlet} from "react-router-dom";
+
+export default function Categories() {
+  return (
+    <div>
+      <h1>categories Page</h1>
+      <div>
+        <Link to="category1">Electronics</Link>
+        <Link to="">Man</Link>
+        <Link to="">Woman</Link>
+        <Link to="category2">Jewel</Link>
+      </div>
+      <div>
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <div>
+      <Link to="/">Card</Link>
+      <Link to="/todo">Todo</Link>
+      <Link to="/controlled_form">Controlled Form</Link>
+      <Link to="/categories">Categories</Link>
+    </div>
+  )
+}
