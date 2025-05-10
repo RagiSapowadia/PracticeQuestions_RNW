b2064467e4e99cfb4da77d47f8e795e0ddc63408
RagiSapowadia <156502099+RagiSapowadia@users.noreply.github.com>
2025-05-10 14:34:47 +0530

    Delete model directory

--- model/authModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const authSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-module.exports = mongoose.model("Auth", authSchema);
